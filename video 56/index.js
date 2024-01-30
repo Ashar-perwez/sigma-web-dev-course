af33b7af4a8299c63195ff1864ed4fe22bc13c23
Ashar Perwez <asharperwez@gmail.com>
2024-01-30 09:33:40 +0530

    JavaScript Conditionals

---
+++ video 56/index.js
@@ -0,0 +1,40 @@
+console.log("Hello I am conditional tutorial")
+
+// let age = 45;
+
+// if (age > 45){
+//     console.log("You can drive")
+// }
+// else{
+//     console.log("You cannot drive")
+// }
+
+let age = 1;
+let grace = 2;
+age +=grace
+console.log(age)
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+
+/*
+I am a
+multiline 
+comment
+*/
+
+a = 6;
+b = 8;
+let c = a > b ? (a-b) : (b-a);
+
+/*
+translate to:
+if(a>b){
+    let c = a-b;
+}
+else{
+    let c = a-b;
+}
+*/
