7ffa332fa3c46ef5c44088c8c6e07aa1d9d7434b
Ashar Perwez <asharperwez@gmail.com>
2024-02-29 12:15:18 +0530

    Events and other DOM properties explored

---
+++ video 71/index.js
@@ -0,0 +1,18 @@
+// let div = document.createElement('div');
+// div.innerHTML='I have been inserted <b> by Ashar</b>'
+// div.setAttribute("class", "created");
+// document.querySelector(".container").after(div)
+
+
+
+// let div= document.createElement('div');
+// div.insertAdjacentHTML('beforebegin', <p> Hello</p>);
+// div.insertAdjacentHTML('afterend', <p> Bye</p>);
+
+// let cont = document.querySelector(".container")
+//             cont.insertAdjacentHTML("beforebegin", "<b> I am under the water. Please h elp me here too much raining.... iuuuuooooo</b>")
+//             cont.insertAdjacentHTML('afterend', '<p> Bye</p>');
+
+       let cont = document.querySelector(".container")
+cont.insertAdjacentHTML('beforebegin', '<p> Hello</p>');
+cont.insertAdjacentHTML('afterend', '<p> Bye</p>');
